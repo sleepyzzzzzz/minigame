86f255bec94c5875585697ecc639fe3146c13e48
Chris Zhou <chrisz881711@gmail.com>
2019-05-15 01:33:27 +0800

    chara

---
+++ Assets/Resources/Scripts/Character/PlayerControl.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody2D rig;
+
+    private float JumpForce;
+    public float speed = 30f;
+    public float crouch_speed = 0.5f;
+    public float current_speed = 0f;
+    public float horizontal = 0.0f;
+    public bool jump = false;
+    public bool crouch = false;
+
+    private bool isGround = true;
+    public Transform target_pos;
+    public float checkr;
+    public LayerMask whatisGround;
+
+    void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        isGround = Physics2D.OverlapCircle(target_pos.position, checkr, whatisGround);
+        horizontal = Input.GetAxis("Horizontal");
+        current_speed = horizontal * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.W) && isGround)
+        {
+            jump = true;
+            rig.velocity = Vector2.up * JumpForce;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            crouch = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            crouch = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        MOVE_AD();
+        JUMP();
+        CROUCH();
+    }
+
+    public void MOVE_AD ()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+
+    public void JUMP ()
+    {
+        if (isGround && jump)
+        {
+            rig.AddForce(new Vector2(0, JumpForce));
+            isGround = false;
+        }
+    }
+
+    public void CROUCH ()
+    {
+        current_speed *= crouch_speed;
+        rig.velocity *= current_speed;
+    }
+}
---
+++ Assets/Resources/Scripts/Character/PlayerPlace.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlace : MonoBehaviour {
+
+    public GameObject blue_door;
+    public GameObject red_door;
+
+    private void Awake()
+    {
+        blue_door = GameObject.Find("Blue_door");
+        red_door = GameObject.Find("Red_door");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(blue_door, transform.position, Quaternion.identity);
+        }
+        if (Input.GetMouseButton(1))
+        {
+            Instantiate(red_door, transform.position, Quaternion.identity);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            red_door.transform.parent = this.transform;
+            red_door.GetComponent<Rigidbody2D>().isKinematic = true;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 position = Input.mousePosition;
+            red_door.GetComponent<Rigidbody2D>().isKinematic = false;
+            transform.DetachChildren();
+            Vector3 dir = transform.TransformDirection(position);
+            red_door.GetComponent<Rigidbody2D>().AddForce(dir, ForceMode2D.Impulse);
+        }
+    }
+}
