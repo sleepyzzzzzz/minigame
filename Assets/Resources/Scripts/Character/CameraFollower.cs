889f5ac3e38d8e2458a0b0c501eea3ca4f2c6a76
Chris Zhou <chrisz881711@gmail.com>
2019-05-15 15:00:32 +0800

    c

---
+++ Assets/Resources/Scripts/Character/CameraFollower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour {
+    public Transform player;
+    public float smooth = 3.5f;
+    public float XAxis;
+
+    private void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        if (Mathf.Abs(pos.x - player.position.x) > XAxis)
+        {
+            pos.x = Mathf.Lerp(pos.x, player.position.x, smooth * Time.deltaTime);
+        }
+        transform.position = new Vector3(pos.x, transform.position.y, transform.position.z);
+    }
+}
