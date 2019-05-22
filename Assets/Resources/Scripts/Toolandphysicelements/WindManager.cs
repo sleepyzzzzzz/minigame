cbe5922c66c4e1b4d3a3f538dadfdcb114145703
xqqxiaoqiqi <1577232763@qq.com>
2019-05-22 20:06:24 +0800

    简单的关于风力的规定
    在windmanager里的方法生成/取消风力。

---
+++ Assets/Resources/Scripts/Toolandphysicelements/WindManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace wind
+{
+    /// <summary>定义了风力的大小和生成
+    /// 
+    /// </summary>
+    public class WindManager : MonoBehaviour
+    {
+        private Vector3 wind;
+        /// <summary>根据传入的vector写入wind
+        /// 
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <returns></returns>
+        public Vector3 CreateWind(Vector3 vector)
+        {
+            wind = vector;
+            return vector;
+        }
+        /// <summary>取消场景内风力作用
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public bool StopWind()
+        {
+            wind = Vector3.zero;
+            return true;
+        }
+    }
+}
+
