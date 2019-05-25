8ebde541c0918a7bf2fd139e4ca6d1e2663f31cc
Chris Zhou <chrisz881711@gmail.com>
2019-05-26 01:55:55 +0800

    character_test3

--- Assets/Character_Test/Scripts/PlayerController.cs
+++ Assets/Character_Test/Scripts/PlayerController.cs
@@ -8,11 +8,15 @@ public enum Player_State
     Dead
 }
 
+public enum Collision_Object
+{
+    SmallChalk
+}
+
 namespace Controller
 {
     public class PlayerController : MonoBehaviour
     {
-
         private Rigidbody2D player;
         private Animator player_animator;
         private float speed = 4.5f;
@@ -21,8 +25,12 @@ namespace Controller
         public static bool facing_right = true;
         public static bool facing_left = false;
 
+        private int first_chalk_hit_count = 0;
+
         private bool walk;
+        private bool hurt;
         Player_State State = Player_State.Alive;
+        Collision_Object Collide = Collision_Object.SmallChalk;
 
         // Use this for initialization
         void Awake()
@@ -36,12 +44,14 @@ namespace Controller
         {
             switch (State)
             {
-                case Player_State.Dead:
-                    player_animator.SetFloat("speed", 0);
-                    walk = false;
-                    break;
                 case Player_State.Alive:
                     Action();
+                    break;
+                case Player_State.Dead:
+                    player_animator.SetBool("fail", true);
+                    //player_animator.SetBool("running", false);
+                    //player_animator.SetBool("hurt", false);
+                    //player_animator.SetFloat("verti", 0.0f);
                     break;
             }
         }
@@ -72,30 +82,11 @@ namespace Controller
                 facing_left = false;
                 transform.Translate(Vector3.right * speed * Time.deltaTime);
             }
-            Move_Anim();
             if (Input.GetKey(KeyCode.W) && isGrounded())
             {
                 player.AddForce(new Vector2(0, JumpForce));
             }
-            Jump_Anim(vertical);
-        }
-
-        void Move_Anim()
-        {
-            if (walk)
-            {
-                player_animator.SetBool("running", true);
-                walk = false;
-            }
-            else
-            {
-                player_animator.SetBool("running", false);
-            }
-        }
-
-        void Jump_Anim(float vertical)
-        {
-            player_animator.SetFloat("verti", vertical);
+            Move_Anim(vertical);
         }
 
         void Flip()
@@ -111,6 +102,72 @@ namespace Controller
             Vector2 start = new Vector2(bounds.center.x, bounds.min.y - bounds.size.y * 0.1f);
             RaycastHit2D hit = Physics2D.Linecast(start, bounds.center);
             return (hit.collider.gameObject != gameObject);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            switch (Collide)
+            {
+                case Collision_Object.SmallChalk:
+                    var tag = collision.collider.tag;
+                    if (tag == "SmallChalk")
+                    {
+                        first_chalk_hit_count++;
+                        player_animator.SetBool("hurt", true);
+                    }
+                    break;
+            }
+        }
+
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            switch (Collide)
+            {
+                case Collision_Object.SmallChalk:
+                    var tag = collision.collider.tag;
+                    if (tag == "SmallChalk")
+                    {
+                        player_animator.SetBool("hurt", true);
+                    }
+                    break;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            switch (Collide)
+            {
+                case Collision_Object.SmallChalk:
+                    var tag = collision.collider.tag;
+                    if (tag != "SmallChalk")
+                    {
+                        player_animator.SetBool("hurt", false);
+                        if (first_chalk_hit_count == 3)
+                        {
+                            State = Player_State.Dead;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        void Move_Anim(float vertical)
+        {
+            if (walk)
+            {
+                player_animator.SetBool("running", true);
+                walk = false;
+            }
+            else
+            {
+                player_animator.SetBool("running", false);
+            }
+            Jump_Anim(vertical);
+        }
+
+        void Jump_Anim(float vertical)
+        {
+            player_animator.SetFloat("verti", vertical);
         }
     }
 }
