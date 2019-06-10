d439889cdf0366beb98133338d42fb08473e0706
Chris Zhou <chrisz881711@gmail.com>
2019-06-10 18:39:54 +0800

    Update SecondBoss.cs

--- Assets/SecondBoss.cs
+++ Assets/SecondBoss.cs
@@ -7,6 +7,8 @@ public class SecondBoss : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
 
     // Update is called once per frame
     void Update()
