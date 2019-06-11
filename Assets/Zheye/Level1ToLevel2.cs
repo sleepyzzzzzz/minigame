8181f28cc76b6281477e77146089c89a7fc5ad79
CaoZhefu <CaoZhefu@github.com>
2019-06-11 22:56:47 +0800

    aaa

---
+++ Assets/Zheye/Level1ToLevel2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1ToLevel2 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag=="Player")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Level1—boss");
+        }
+    }
+}
