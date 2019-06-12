53f995669865c03432e02045ceacd47dad8beeec
CaoZhefu <CaoZhefu@github.com>
2019-06-12 17:50:36 +0800

    Level3boss0.5

---
+++ Assets/Zheye/ThirdBoss/PaperPlane.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperPlane : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.collider.tag)
+        {
+            case "Player":break;
+            case "ground":break;
+            default:break;
+        }
+
+    }
+}
