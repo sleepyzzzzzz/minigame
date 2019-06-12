33bd7f833846005ce535313788152473a243ab5d
CaoZhefu <CaoZhefu@github.com>
2019-06-12 12:12:34 +0800

    FixPlayerFacingBug

--- Assets/Zheye/FirstBoss/Book.cs
+++ Assets/Zheye/FirstBoss/Book.cs
@@ -6,7 +6,6 @@ public class Book : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D coll)
     {
-        Debug.Log("Book Collision:" + coll.collider.tag);
         switch (coll.collider.tag)
         {
             case "ground":
