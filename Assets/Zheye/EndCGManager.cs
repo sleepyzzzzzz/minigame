eb9a36f4009db3f46b61d18453da03a07bb9ceaf
CaoZhefu <CaoZhefu@github.com>
2019-06-12 22:31:36 +0800

    EndCG

--- Assets/Zheye/EndCGManager.cs
+++ Assets/Zheye/EndCGManager.cs
@@ -11,16 +11,11 @@ namespace CZF
         /// StartCG
         /// </summary>
         private Image[] CGImages;
-        public string[] CGTexts = new string[4] { "这是第一张图片", "这是第二张图片", "这是第三张图片", "这是第四张图片" };
 
         public float ImageFadeTime = 1f;
-
-        public float PhraseShowTime = 1f;
-        public float TextFadeTime = 2f;//一行字消失时间
-        public float SwitchLineTime = 0.8f;//每行字切换间隔时间
-        public float TextExistTime = 1f;//每行字停留时间
-
-        public Text ShowText;
+        public float ImageShowTime = 1f;
+        public float SwitchImageTime = 0.8f;
+        public float ImageExistTime = 1f;
         private Image mask;
 
         void Start()
@@ -28,7 +23,6 @@ namespace CZF
             mask = transform.Find("mask").GetComponent<Image>();
             CGImages = transform.Find("CGAnim").GetComponentsInChildren<Image>();
             StartCoroutine(ShowStartCG());
-            StartCoroutine(ShowCGText());
         }
 
 
@@ -36,10 +30,11 @@ namespace CZF
         {
             for (int i = 0; i < CGImages.Length; i++)
             {
-                StartCoroutine(ImageAlphaAnim(CGImages[i], 1, PhraseShowTime));
-                yield return new WaitForSeconds(PhraseShowTime + TextExistTime + TextFadeTime + SwitchLineTime);
+                StartCoroutine(ImageAlphaAnim(CGImages[i], 1, ImageShowTime));
+                yield return new WaitForSeconds(ImageShowTime + ImageExistTime + SwitchImageTime);
             }
             StartCoroutine(ImageAlphaAnim(mask, 1, ImageFadeTime));
+            yield return new WaitForSeconds(ImageFadeTime);
             UnityEngine.SceneManagement.SceneManager.LoadScene("StartMenu");
         }
 
@@ -57,36 +52,6 @@ namespace CZF
                 yield return 0;
             }
             image.color = new Color(image.color.r, image.color.g, image.color.b, EndValue);
-        }
-
-        //开始显示文字的协程
-        IEnumerator ShowCGText()
-        {
-            for (int i = 0; i < CGTexts.Length; i++)
-            {
-                ShowText.text = CGTexts[i];
-                StartCoroutine(TextAlphaAnim(ShowText, 1, PhraseShowTime));
-                yield return new WaitForSeconds(PhraseShowTime + TextExistTime);
-                StartCoroutine(TextAlphaAnim(ShowText, 0, TextFadeTime));
-                yield return new WaitForSeconds(TextFadeTime + SwitchLineTime);
-            }
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Main");
-        }
-
-
-        //字符透明度动画
-        IEnumerator TextAlphaAnim(Text text, float EndValue, float time)
-        {
-            int timer = 0;
-            int frameCount = (int)(time / Time.fixedDeltaTime);
-            float Step = (EndValue - text.color.a) / frameCount;
-            while (timer < frameCount)
-            {
-                text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + Step);
-                timer++;
-                yield return 0;
-            }
-            text.color = new Color(text.color.r, text.color.g, text.color.b, EndValue);
         }
     }
 }
